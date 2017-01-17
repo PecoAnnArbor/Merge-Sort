daa528e2d80746ac518ff594e9118511accc4bc1
Ian Zhang <ianzhang3374@gmail.com>
2017-01-17 09:23:20 -0500

    Add files via upload

---
+++ mergeSort.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+
+using namespace std;
+
+void merge(int* a, int* b, int low, int high, int pivot) {
+	int h, i, j, k;
+	h = low;
+	i = low;
+	j = pivot+1;
+	while(h <= pivot && j <= high ) {
+		if(a[h] < a[j]) {
+			b[i] = a[h];
+			h++;
+		}
+		else{
+			b[i] = a[j];
+			j++;
+		}
+		i++;
+	}
+	if(h > pivot) {
+		for(k = j; k <= high; k++) {
+			b[i] = a[k];
+			i++;
+		}
+	}
+	else{
+		for(k = h; k <= pivot; k++) {
+			b[i] = a[k];
+			i++;
+		}
+	}
+	for(k=low; k<=high; k++) {
+        a[k]=b[k];
+    }
+}
+
+void mergesort(int* a, int* b, int low, int high) {
+	int pivot;
+	if(low < high) {
+		pivot = (low + high)/2;
+		mergesort(a, b, low, pivot);
+		mergesort(a, b, pivot+1, high);
+		merge(a, b, low, high, pivot);
+	}
+}
+
+int main() {
+	int a[] = {12,10,43,23,78,45,123,56,98,41,90,24};
+	int num;
+	num = sizeof(a)/sizeof(a[0]);
+	int b[num];
+	mergesort(a, b, 0, num-1);
+	for(int counter = 0; counter < num; counter++) {
+		cout << a[counter] << " ";
+	}
+	cout << endl;
+}
